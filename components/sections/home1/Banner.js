bf36d8044e8db112d21e335e10c3416378cbeb4c
parag <paragakabari@gmail.com>
2024-10-08 14:28:39 +0530

    testing font

--- components/sections/home1/Banner.js
+++ components/sections/home1/Banner.js
@@ -37,7 +37,7 @@ export default function Banner() {
                             <div className="sub-title">
                             <TypingAnimation />
                             </div>
-                            <h2 style={{fontFamily:"cursive"}}>
+                            <h2>
                             Brand, Design, Product, <br />
                             In-house Development & More
                                 {/* Digital Agency<br/>For Growth */}
